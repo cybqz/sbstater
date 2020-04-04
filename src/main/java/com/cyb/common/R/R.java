561318fc34fef18f2f3fc992aa118646e8c7d0c3
cybqzwork <cybqzwork@163.com>
2020-04-04 14:28:35 +0800

    添加R

---
+++ src/main/java/com/cyb/common/R/R.java
@@ -0,0 +1,33 @@
+package com.cyb.common.R;
+
+import lombok.Data;
+
+/**
+ * 公共返回结果封装
+ */
+@Data
+public class R {
+
+    private String msg;
+
+    private boolean result = false;
+
+    private R(String msg) {
+        this.msg = msg;
+    }
+
+    public static R success(String msg){
+        R r = new R(msg);
+        r.setResult(true);
+        return r;
+    }
+
+    public static R fail(String msg){
+        R r = new R(msg);
+        return r;
+    }
+
+    public boolean isSuccess(){
+        return this.result;
+    }
+}
