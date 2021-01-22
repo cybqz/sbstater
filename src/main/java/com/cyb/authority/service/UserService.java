e0944d81cd1325ca9936d4d9a8f441ba2809f97d
CYB <work123>
2021-01-22 21:19:56 +0800

    完善方法及逻辑

--- src/main/java/com/cyb/authority/service/UserService.java
+++ src/main/java/com/cyb/authority/service/UserService.java
@@ -1,16 +1,21 @@
 package com.cyb.authority.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cyb.authority.constant.SexEnum;
 import com.cyb.authority.dao.UserMapper;
 import com.cyb.authority.domain.User;
 import com.cyb.authority.utils.EncryptionDecrypt;
+import com.cyb.common.pagination.Pagination;
 import com.cyb.common.tips.Tips;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * @Author 陈迎博
@@ -42,7 +47,7 @@ public class UserService extends ServiceImpl<UserMapper, User> {
 			image += "boy.png";
 		}
 		record.setImage(image);
-
+		record.setCreateDateTime(LocalDateTime.now());
 		return userMapper.insert(record);
 	}
 
@@ -70,6 +75,7 @@ public class UserService extends ServiceImpl<UserMapper, User> {
 				tips.setMsg("新密码不能和旧密码相同");
 			}else{
 				user.setPassword(encPassword);
+				user.setCreateDateTime(LocalDateTime.now());
 				int count = userMapper.updateById(user);
 				if(count > 0){
 					if(againLogin){
@@ -82,7 +88,50 @@ public class UserService extends ServiceImpl<UserMapper, User> {
 		return tips;
 	}
 
+	/**
+	 * @Author 陈迎博
+	 * @Title 根据用户姓名查询用户信息
+	 * @Description
+	 * @Date 2021/1/22
+	 */
 	public User selectByUserName(String userName) {
 		return userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getUserName, userName));
+	}
+
+	/**
+	 * @Author 陈迎博
+	 * @Title 分页查询
+	 * @Description 分页查询
+	 * @Date 2021/1/16
+	 */
+	public IPage<User> selectPage(User record, Pagination pagination) {
+
+		LambdaQueryWrapper<User> queryWrapper = queryWrapper = new LambdaQueryWrapper<User>();
+		queryWrapper.orderByDesc(User::getCreateDateTime);
+		if(null != record){
+			queryWrapper = queryWrapper.like(StringUtils.isNotBlank(record.getUserName()), User::getUserName, record.getUserName());
+		}
+
+		Page page = null;
+		if(null != pagination){
+			page = new Page(pagination.getPageIndex(), pagination.getLimit());
+		}
+		return userMapper.selectPage(page, queryWrapper);
+	}
+
+	/**
+	 * @Author 陈迎博
+	 * @Title 根据条件查询用户总数
+	 * @Description
+	 * @Date 2021/1/22
+	 */
+	public Integer selectCount(User user){
+
+		return userMapper.selectCount(new QueryWrapper<User>().lambda()
+				.like(StringUtils.isNotBlank(user.getName()), User::getName, user.getName())
+				.like(StringUtils.isNotBlank(user.getEmail()), User::getEmail, user.getEmail())
+				.like(StringUtils.isNotBlank(user.getPhone()), User::getPhone, user.getPhone())
+				.like(StringUtils.isNotBlank(user.getUserName()), User::getUserName, user.getUserName())
+		);
 	}
 }
