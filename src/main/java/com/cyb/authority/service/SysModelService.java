64de16482077bfb06f08f705b4d69c92498af94e
CYB <work123>
2021-02-08 23:20:03 +0800

    优化代码逻辑

--- src/main/java/com/cyb/authority/service/SysModelService.java
+++ src/main/java/com/cyb/authority/service/SysModelService.java
@@ -7,14 +7,19 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cyb.authority.dao.SysModelMapper;
 import com.cyb.authority.domain.SysModel;
 import com.cyb.authority.domain.UserSysModel;
+import com.cyb.authority.vo.SysModelVO;
 import com.cyb.common.pagination.Pagination;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -110,6 +115,7 @@ public class SysModelService extends ServiceImpl<SysModelMapper, SysModel> {
         if(StringUtils.isNotBlank(userId)){
 
             LambdaQueryWrapper<SysModel> queryWrapper = new LambdaQueryWrapper<SysModel>()
+                    .orderByAsc(SysModel::getSort)
                     .orderByDesc(SysModel::getCreateDateTime);
 
             //查询用户已拥有的系统模块
@@ -130,6 +136,70 @@ public class SysModelService extends ServiceImpl<SysModelMapper, SysModel> {
 
     /**
      * @Author 陈迎博
+     * @Title 分页查询用户已拥有系统模块
+     * @Description 分页查询用户已拥有系统模块
+     * @Date 2021/1/16
+     */
+    public List<SysModelVO> selectListHav(String userId) {
+
+        List<SysModelVO> resultList = null;
+        if(StringUtils.isNotBlank(userId)){
+
+            LambdaQueryWrapper<SysModel> queryWrapper = new LambdaQueryWrapper<SysModel>()
+                    .orderByAsc(SysModel::getSort)
+                    .orderByDesc(SysModel::getCreateDateTime);
+
+            //查询用户已拥有的系统模块
+            List<UserSysModel> userSysModelList = userSysModelService.selectBySysModelId(userId);
+            if(!CollectionUtils.isEmpty(userSysModelList)){
+
+                List<String> sysModelIds = userSysModelList.stream().map(UserSysModel::getSysModelId).collect(Collectors.toList());
+                queryWrapper.in(SysModel::getId, sysModelIds);
+                List<SysModel> sysModelList = sysModelMapper.selectList(queryWrapper);
+                if(!CollectionUtils.isEmpty(sysModelList)){
+
+                    resultList = new ArrayList<SysModelVO>();
+                    List<SysModel> parentList = new ArrayList<SysModel>();
+                    //添加父栏目
+                    Map<String, List<SysModel>> tempMap = new HashMap<String, List<SysModel>>();
+                    for(SysModel sysModel : sysModelList){
+                        if(StringUtils.isBlank(sysModel.getParentId())){
+                            if(!tempMap.containsKey(sysModel.getId())){
+                                parentList.add(sysModel);
+                                tempMap.put(sysModel.getId(), new ArrayList<SysModel>());
+                            }
+                        }
+                    }
+
+                    //添加子栏目
+                    for(SysModel sysModel : sysModelList){
+                        String key = sysModel.getParentId();
+                        if(StringUtils.isNotBlank(key)){
+                            if(tempMap.containsKey(key)){
+                                List<SysModel> list = tempMap.get(key);
+                                list.add(sysModel);
+                                tempMap.put(key, list);
+                            }
+                        }
+                    }
+
+                    //组装返回结果
+                    for(SysModel sysModel : parentList){
+                        if(tempMap.containsKey(sysModel.getId())){
+                            SysModelVO sysModelVO = new SysModelVO();
+                            BeanUtils.copyProperties(sysModel, sysModelVO);
+                            sysModelVO.setChildren(tempMap.get(sysModel.getId()));
+                            resultList.add(sysModelVO);
+                        }
+                    }
+                }
+            }
+        }
+        return resultList;
+    }
+
+    /**
+     * @Author 陈迎博
      * @Title 分页查询用户未拥有系统模块
      * @Description 分页查询用户未拥有系统模块
      * @Date 2021/1/16
@@ -139,6 +209,7 @@ public class SysModelService extends ServiceImpl<SysModelMapper, SysModel> {
         if(StringUtils.isNotBlank(userId)){
 
             LambdaQueryWrapper<SysModel> queryWrapper = new LambdaQueryWrapper<SysModel>()
+                    .orderByAsc(SysModel::getSort)
                     .orderByDesc(SysModel::getCreateDateTime);
 
             //查询用户已拥有的系统模块
@@ -165,8 +236,9 @@ public class SysModelService extends ServiceImpl<SysModelMapper, SysModel> {
      */
     public IPage<SysModel> selectPage(SysModel record, Pagination pagination) {
 
-        LambdaQueryWrapper<SysModel> queryWrapper = queryWrapper = new LambdaQueryWrapper<SysModel>();
-        queryWrapper.orderByAsc(SysModel::getSort).orderByDesc(SysModel::getCreateDateTime);
+        LambdaQueryWrapper<SysModel> queryWrapper = queryWrapper = new LambdaQueryWrapper<SysModel>()
+                .orderByAsc(SysModel::getSort)
+                .orderByDesc(SysModel::getCreateDateTime);
         if(null != record){
             queryWrapper = queryWrapper.like(StringUtils.isNotBlank(record.getUrl()), SysModel::getUrl, record.getUrl());
             queryWrapper = queryWrapper.like(StringUtils.isNotBlank(record.getTitle()), SysModel::getTitle, record.getTitle());
@@ -195,6 +267,8 @@ public class SysModelService extends ServiceImpl<SysModelMapper, SysModel> {
             queryWrapper = queryWrapper.like(StringUtils.isNotBlank(record.getTitle()), SysModel::getTitle, record.getTitle())
                     .like(StringUtils.isNotBlank(record.getNavbar()), SysModel::getNavbar, record.getNavbar());
         }
+
+        //url为空则是父栏目
         queryWrapper = queryWrapper.and(wrapper -> wrapper.isNull(SysModel::getUrl).or().eq(SysModel::getUrl,""));
         Page page = null;
         if(null != pagination){
---
+++ src/main/java/com/cyb/authority/vo/SysModelVO.java
@@ -0,0 +1,26 @@
+package com.cyb.authority.vo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.cyb.authority.domain.SysModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @Author 陈迎博
+ * @Description 系统模块对象VO
+ * @Date 2021/2/8
+ */
+@Getter
+@Setter
+public class SysModelVO {
+
+    private String title;
+
+    private String navbar;
+
+    private String url;
+
+    private List<SysModel> children;
+}
