45270030827d31f6d7f49f459ab621f43091a04f
CYB <work123>
2021-02-13 11:48:36 +0800

    添加AddValid、UpdateValid类

---
+++ src/main/java/com/cyb/common/validation/AddValid.java
@@ -0,0 +1,9 @@
+package com.cyb.common.validation;
+
+/**
+ * @Author 陈迎博
+ * @Description 新增校验分组
+ * @Date 2021/2/13
+ */
+public class AddValid {
+}
---
+++ src/main/java/com/cyb/common/validation/UpdateValid.java
@@ -0,0 +1,9 @@
+package com.cyb.common.validation;
+
+/**
+ * @Author 陈迎博
+ * @Description 更新校验分组
+ * @Date 2021/2/13
+ */
+public interface UpdateValid {
+}
