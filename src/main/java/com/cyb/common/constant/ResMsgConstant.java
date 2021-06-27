0fc5aedd766ac209a5ec59f59438ec8edbb54368
CYB <work123>
2021-06-27 19:38:12 +0800

    添加ResMsgConstant类

--- src/main/java/com/cyb/common/constant/ResMsgConstant.java
+++ src/main/java/com/cyb/common/constant/ResMsgConstant.java
@@ -7,4 +7,8 @@ package com.cyb.common.constant;
  */
 public class ResMsgConstant {
     public static String REQUEST_IS_NULL = "请求对象是空！";
+
+    public static String QUERY_FAIL = "查询失败！";
+
+    public static String QUERY_SUCCESS = "查询成功！";
 }
