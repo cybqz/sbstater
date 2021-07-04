f8c51b2c56a9be98456bbbfc341025f98fd9c93b
CYB <work123>
2021-07-04 14:40:50 +0800

    添加BaseDomain类

---
+++ src/main/java/com/cyb/common/domain/BaseDomain.java
@@ -0,0 +1,37 @@
+package com.cyb.common.domain;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.cyb.common.validation.group.UpdateValid;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+/**
+ * 基础Domain类
+ * @author CYB
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseDomain {
+
+    @TableId(value = "id")
+    @ApiModelProperty(value = "ID")
+    @NotBlank(message = "权限编号不能为空", groups = {UpdateValid.class})
+    private String id;
+
+    @TableField(value = "create_date_time")
+    @ApiModelProperty(value = "createDateTime")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createDateTime;
+
+    @TableField(value = "update_date_time")
+    @ApiModelProperty(value = "updateDateTime")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime updateDateTime;
+}
