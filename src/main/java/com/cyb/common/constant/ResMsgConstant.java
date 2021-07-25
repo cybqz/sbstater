8bd2f7f6a9c633f73a8edac1d9eee704cfab8d1c
CYB <work123>
2021-07-25 14:35:41 +0800

    添加提示语

--- src/main/java/com/cyb/common/constant/ResMsgConstant.java
+++ src/main/java/com/cyb/common/constant/ResMsgConstant.java
@@ -8,11 +8,20 @@ package com.cyb.common.constant;
 public class ResMsgConstant {
     public static String REQUEST_IS_NULL = "请求对象是空！";
 
-    public static String QUERY_FAIL = "查询失败！";
-    public static String QUERY_SUCCESS = "查询成功！";
-
     public static String ADD_FAIL = "添加失败！";
     public static String ADD_SUCCESS = "添加成功！";
 
+    public static String DELETE_FAIL = "删除失败！";
+    public static String DELETE_SUCCESS = "删除成功！";
+
+    public static String UPDATE_FAIL = "更新失败！";
+    public static String UPDATE_SUCCESS = "更新成功！";
+    public static String UPDATE_FAIL_ORI_DATA_NOT_EXISTENT = "更新失败，原始数据不存在！";
+
+    public static String QUERY_FAIL = "查询失败！";
+    public static String QUERY_SUCCESS = "查询成功！";
+
+
+    public static String DATA_UNIQUE_ID_FAIL = "数据唯一ID不存在！";
     public static String DATA_UNIQUE_VALID_FAIL = "数据唯一性校验失败，相同数据已存在！";
 }
